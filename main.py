2cde15b083b9d00a9a7034e191696531022ae655
Aseem Srivastava <aseemsrivastava9@gmail.com>
2022-12-18 23:44:34 +0530

    Added Basic functionality of Snake and Comments
    Added sleep and update options for screen
    Added loop to make snake body follow snake head

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)  # Turn of turtle animation
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]  # List of starting positions of turtles
+
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()  # Update the screen
+    time.sleep(0.1)  # Sleep for 0.1 sec
+    for seg_num in range(len(segments) - 1, 0, -1):  # Loop for making all segments follow the snake head
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
