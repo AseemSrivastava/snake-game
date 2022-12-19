b75ac7c543fe2b0468b59c8b2e03397c81fe90c3
Aseem Srivastava <aseemsrivastava9@gmail.com>
2022-12-19 23:53:18 +0530

    Added oops and separate file for snake
    Added oops and separate file for snake controls.
    Added Comments.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
+
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,25 +9,19 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)  # Turn of turtle animation
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]  # List of starting positions of turtles
+snake = Snake()
 
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+# Up, Down, Left, Right represent the corresponding arrow keys on keyboard
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()  # Update the screen
     time.sleep(0.1)  # Sleep for 0.1 sec
-    for seg_num in range(len(segments) - 1, 0, -1):  # Loop for making all segments follow the snake head
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
