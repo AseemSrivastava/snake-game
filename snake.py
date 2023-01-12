a2f9027fa49d747ea8ff774183e64f27ae1901a8
Aseem Srivastava <aseemsrivastava9@gmail.com>
2023-01-12 22:35:38 +0530

    Highscore
    Added a option to show high score during current session of game.

--- snake.py
+++ snake.py
@@ -19,7 +19,7 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
+    # Create new segment and add it to end of snake
     def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
@@ -27,7 +27,14 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
-    # Add new segment to snake
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # Add new segment to snake by calling add segment method
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
